3f40d843b7c41773cb2f7df445c1a2e0ce48bb1a
dcaruso19 <dcaruso19@gmail.com>
2015-02-09 18:58:02 -0500

    first commit. added override functions with respective bases copied from Wizzrobe.

---
+++ King of Thieves/Actors/NPC/Enemies/Rope/CBaseRope.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace King_of_Thieves.Actors.NPC.Enemies.Rope
+{
+    enum ROPEETYPE
+    {
+        NORMAL = 0
+    
+    }
+
+    class CBaseRope : CBaseEnemy
+    {
+        public CBaseRope(int foh, params dropRate[] drops)
+            : base(drops)
+        {
+        }
+
+        public override void update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            base.update(gameTime);
+        }
+
+        public override void create(object sender)
+        {
+            base.create(sender);
+
+        }
+
+        public override void destroy(object sender)
+        {
+         
+            base.destroy(sender);
+        }
+
+        protected override void cleanUp()
+        { }
+
+        public override void timer0(object sender)
+        {
+            base.timer0(sender);
+        }
+
+        public override void timer1(object sender)
+        {
+            base.timer1(sender);
+            
+        }
+
+        public override void timer2(object sender)
+        {
+            base.timer2(sender);
+        }
+
+        public override void timer3(object sender)
+        {
+            base.timer3(sender);
+        }
+
+        public override void drawMe(bool useOverlay = false)
+        {
+            
+                base.drawMe(useOverlay);
+        }
+
+      
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+     
+}
---
+++ King of Thieves/Actors/NPC/Enemies/Rope/CRope.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace King_of_Thieves.Actors.NPC.Enemies.Rope
+{
+    class CRope : CBaseRope
+    {
+
+        public CRope()
+            : base(60, new dropRate(new Items.Drops.CHeartDrop(), 100))
+        {
+            _type = ROPETYPE.NORMAL;
+        }
+
+    }
+}
