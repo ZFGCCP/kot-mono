33f60d60be0cc2585611806e1f8bcbccad2c4773
dcaruso19 <dcaruso19@gmail.com>
2015-03-21 17:30:18 -0400

    added  8 slither and fast slither movements with proper speeds and images

--- King of Thieves/Actors/NPC/Enemies/Rope/CBaseRope.cs
+++ King of Thieves/Actors/NPC/Enemies/Rope/CBaseRope.cs
@@ -14,7 +14,7 @@ namespace King_of_Thieves.Actors.NPC.Enemies.Rope
 
     class CBaseRope : CBaseEnemy
     {
-        protected const string _NPC_ROPE = "npc:rope";
+        protected const string SPRITE_NAMESPACE = "npc:rope";
         private static int _ropeCount = 0;
         private static int _greenRopeCount = 0;
 
@@ -34,12 +34,43 @@ namespace King_of_Thieves.Actors.NPC.Enemies.Rope
         public CBaseRope(int foh, params dropRate[] drops)
             : base(drops)
         {
-            if (!Graphics.CTextures.rawTextures.ContainsKey(_NPC_ROPE))
-                Graphics.CTextures.rawTextures.Add(_NPC_ROPE, CMasterControl.glblContent.Load<Texture2D>(@"sprites/npc/rope"));
+            if (!Graphics.CTextures.rawTextures.ContainsKey(SPRITE_NAMESPACE))
+            {
+
+                Graphics.CTextures.rawTextures.Add(SPRITE_NAMESPACE, CMasterControl.glblContent.Load<Texture2D>(@"sprites/npc/rope"));
 
 
-            _ropeCount += 1;
+                _ropeCount += 1;
+
+
+                Graphics.CTextures.addTexture(_SLITHER_DOWN, new Graphics.CTextureAtlas(SPRITE_NAMESPACE, 32, 32, 1, "0:1", "3:1", 2));
+                Graphics.CTextures.addTexture(_SLITHER_UP, new Graphics.CTextureAtlas(SPRITE_NAMESPACE, 32, 32, 1, "0:2", "3:2", 2));
+                Graphics.CTextures.addTexture(_SLITHER_LEFT, new Graphics.CTextureAtlas(SPRITE_NAMESPACE, 32, 32, 1, "0:0", "0:3", 2));
+               
+
+
+                Graphics.CTextures.addTexture(_FAST_SLITHER_DOWN, new Graphics.CTextureAtlas(SPRITE_NAMESPACE, 32, 32, 1, "0:1", "3:1", 4));
+                Graphics.CTextures.addTexture(_FAST_SLITHER_UP, new Graphics.CTextureAtlas(SPRITE_NAMESPACE, 32, 32, 1, "0:2", "3:2", 4));
+                Graphics.CTextures.addTexture(_FAST_SLITHER_LEFT, new Graphics.CTextureAtlas(SPRITE_NAMESPACE, 32, 32, 1, "0:0", "0:3", 4));
+                
+
+            }
+
+
+            _imageIndex.Add(_SLITHER_DOWN, new Graphics.CSprite(_SLITHER_DOWN));
+            _imageIndex.Add(_SLITHER_UP, new Graphics.CSprite(_SLITHER_UP));
+            _imageIndex.Add(_SLITHER_LEFT, new Graphics.CSprite(_SLITHER_LEFT));
+            _imageIndex.Add(_SLITHER_RIGHT, new Graphics.CSprite(_SLITHER_LEFT,true));
+
+
+            _imageIndex.Add(_FAST_SLITHER_DOWN, new Graphics.CSprite(_FAST_SLITHER_DOWN));
+            _imageIndex.Add(_FAST_SLITHER_UP, new Graphics.CSprite(_FAST_SLITHER_UP));
+            _imageIndex.Add(_FAST_SLITHER_LEFT, new Graphics.CSprite(_FAST_SLITHER_LEFT));
+            _imageIndex.Add(_FAST_SLITHER_RIGHT, new Graphics.CSprite(_FAST_SLITHER_LEFT,true));
+
         }
+
+
 
 
               public override void init(string name, Microsoft.Xna.Framework.Vector2 position, string dataType, int compAddress, params string[] additional)
@@ -49,8 +80,8 @@ namespace King_of_Thieves.Actors.NPC.Enemies.Rope
             switch (additional[0])
             {
                 case "G":
-                    _value = 1;
-                    _imageIndex.Add( _SLITHER_DOWN, new Graphics.CSprite(Graphics.CTextures.DROPS_RUPEE_GREEN));
+                    
+                   
                     break;
 
 
