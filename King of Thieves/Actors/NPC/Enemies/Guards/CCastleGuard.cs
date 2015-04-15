ca4fbc5603aa358d1b9e9cb3081ff4b2c03b8216
MGZero <onisephiroth@gmail.com>
2015-04-15 18:24:44 -0400

    starting castle guard
    Signed-off-by: MGZero <onisephiroth@gmail.com>

---
+++ King of Thieves/Actors/NPC/Enemies/Guards/CCastleGuard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace King_of_Thieves.Actors.NPC.Enemies.Guards
+{
+    enum ALERT_LEVEL
+    {
+        NORMAL = 0,
+        ELEVATED
+    }
+
+    class CCastleGuard : CBaseEnemy
+    {
+        private static ALERT_LEVEL _ALERT = ALERT_LEVEL.NORMAL;
+
+        public CCastleGuard() :
+            base()
+        {
+            _angle = 270;
+            _direction = DIRECTION.DOWN;
+        }
+
+
+    }
+}
