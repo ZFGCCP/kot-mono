dc6f3db13dae24d778d6143e559307b58cf57ce4
mgzero <onisephiroth@gmail.com>
2015-05-08 23:43:00 -0400

    notoriety icon
    Signed-off-by: mgzero <onisephiroth@gmail.com>

---
+++ King of Thieves/Actors/HUD/notoriety/CNotorietyIcon.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace King_of_Thieves.Actors.HUD.notoriety
+{
+    enum NOTORIETY_LEVEL
+    {
+        NONE,
+        MEDIUM,
+        HIGH
+    }
+
+    class CNotorietyIcon : CHUDElement
+    {
+        private NOTORIETY_LEVEL _notorietyLevel = NOTORIETY_LEVEL.NONE;
+
+        public CNotorietyIcon()
+        {
+            _fixedPosition.X = 10;
+            _fixedPosition.Y = 200;
+        }
+
+        public void resetNotoriety()
+        {
+            _notorietyLevel = NOTORIETY_LEVEL.NONE;
+        }
+
+        public void raiseNotoriety()
+        {
+            int level = (int)_notorietyLevel;
+            level += 1;
+            _notorietyLevel = (NOTORIETY_LEVEL)level;
+        }
+
+        public void lowerNotoriety()
+        {
+            int level = (int)_notorietyLevel;
+            level -= 1;
+            _notorietyLevel = (NOTORIETY_LEVEL)level;
+        }
+
+        public NOTORIETY_LEVEL notorietyLevel
+        {
+            get
+            {
+                return _notorietyLevel;
+            }
+        }
+    }
+}
