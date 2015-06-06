95a4a0cf7d06c986530d4d750f5b30f1302e3e47
MGZero <onisephiroth@gmail.com>
2015-06-06 16:08:32 -0400

    forgot the actual sprites.
    Signed-off-by: MGZero <onisephiroth@gmail.com>

---
+++ King of Thieves/Actors/NPC/Enemies/Bokoblin/CBokoblin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace King_of_Thieves.Actors.NPC.Enemies.Bokoblin
+{
+    class CBokoblin : CBaseEnemy
+    {
+        private const string _SPRITE_NAMESPACE = "npc:bokoblin";
+
+        public CBokoblin() :
+            base()
+        {
+
+        }
+    }
+}
