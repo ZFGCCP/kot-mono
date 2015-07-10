accbaae7da66e59e8b49fd36b0af5bb1cc7d097e
MGZero <onisephiroth@gmail.com>
2015-07-10 19:22:30 -0400

    Added map swapper
    Signed-off-by: MGZero <onisephiroth@gmail.com>

---
+++ King of Thieves/Actors/Collision/CMapChanger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace King_of_Thieves.Actors.Collision
+{
+    class CMapChanger : CActor
+    {
+        private string _mapName;
+        private Vector2 _playerPosition;
+
+        public CMapChanger() :
+            base()
+        {
+            _hitBox = new CHitBox(this, 0, 0, 16, 16);
+        }
+
+        public override void init(string name, Vector2 position, string dataType, int compAddress, params string[] additional)
+        {
+            double posX = Convert.ToDouble(additional[0]);
+            double posY = Convert.ToDouble(additional[1]);
+            _mapName = additional[2];
+
+            _playerPosition = new Vector2((float)posX, (float)posY);
+
+            base.init(name, position, dataType, compAddress, additional);
+        }
+
+        public override void collide(object sender, CActor collider)
+        {
+            CMasterControl.mapManager.swapMap(_mapName);
+            
+        }
+
+        protected override void _addCollidables()
+        {
+            base._addCollidables();
+            _collidables.Add(typeof(Player.CPlayer));
+        }
+    }
+}
