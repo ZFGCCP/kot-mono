08f952362d26247f86a23ab281e98d4be50d1e02
mgzero <onisephiroth@gmail.com>
2015-07-11 20:52:29 -0400

    BUT WHO WAS DOOR?!
    Signed-off-by: mgzero <onisephiroth@gmail.com>

---
+++ King of Thieves/Actors/Collision/CDoor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace King_of_Thieves.Actors.Collision
+{
+    class CDoor : CActor
+    {
+        private bool _locked = false;
+
+        public CDoor() :
+            base()
+        {
+            _hitBox = new CHitBox(this, 0, 0, 16, 16);
+        }
+    }
+}
