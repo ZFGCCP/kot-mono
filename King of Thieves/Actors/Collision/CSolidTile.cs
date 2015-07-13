61068e2720bae2e4375c5c33456c1599124d0040
MGZero <onisephiroth@gmail.com>
2015-07-13 13:13:17 -0400

    door and solid tile changes
    Signed-off-by: MGZero <onisephiroth@gmail.com>

--- King of Thieves/Actors/Collision/CSolidTile.cs
+++ King of Thieves/Actors/Collision/CSolidTile.cs
@@ -13,6 +13,14 @@ namespace King_of_Thieves.Actors.Collision
             _followRoot = false;
         }
 
+        public override void init(string name, Microsoft.Xna.Framework.Vector2 position, string dataType, int compAddress, params string[] additional)
+        {
+            base.init(name, position, dataType, compAddress, additional);
+
+            if (additional.Length >= 2)
+                _hitBox = new CHitBox(this, 0, 0, Convert.ToInt32(additional[0]), Convert.ToInt32(additional[1]));
+        }
+
         protected override void applyEffects()
         {
             throw new NotImplementedException();
