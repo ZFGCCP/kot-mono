3caa859f709c99e61c4d200184ecd38359c31e07
MGZero <onisephiroth@gmail.com>
2015-08-02 09:52:22 -0400

    mad changes
    Signed-off-by: MGZero <onisephiroth@gmail.com>

---
+++ King of Thieves/Actors/Items/Drops/CFireHydrant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace King_of_Thieves.Actors.Items.Drops
+{
+    class CFireHydrant : CDroppable
+    {
+
+        public CFireHydrant() :
+            base()
+        {
+            _capacity = 0;
+            _yieldMessage = "You got a...! Fire hydrant..? You should bring it to the dog, you promised after all.";
+            _name = "hydrant";
+        }
+
+        protected override void _yieldToPlayer(bool fromChest = false)
+        {
+            base._yieldToPlayer(true);
+        }
+    }
+}
