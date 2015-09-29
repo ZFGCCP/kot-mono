cffb30e8302533183e80d23d566e410c1d80eeb6
MGZero <onisephiroth@gmail.com>
2015-09-29 19:56:29 -0400

    cullboundary
    Signed-off-by: MGZero <onisephiroth@gmail.com>

---
+++ King of Thieves/Actors/HUD/other/CCullBoundary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace King_of_Thieves.Actors.HUD.other
+{
+    class CCullBoundary : CHUDElement
+    {
+        private int _width = 304;
+        private int _height = 224;
+
+        public CCullBoundary() :
+            base()
+        {
+            _fixedPosition.X = -32;
+            _fixedPosition.Y = -32;
+        }
+
+        public bool checkPointWithinBoundary(Vector2 point, Vector2 dimensions)
+        {
+            point *= dimensions;
+            return !(point.X < position.X ||
+                   point.Y < position.Y ||
+                   point.X > position.X + _width ||
+                   point.Y > position.Y + _height);
+
+        }
+    }
+}
