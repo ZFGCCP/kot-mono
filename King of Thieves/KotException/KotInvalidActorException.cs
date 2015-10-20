8431f8f956a0cca9868cca8e48032adff3f0f018
MGZero <onisephiroth@gmail.com>
2015-10-19 22:32:24 -0400

    Added invalidactorexception
    Signed-off-by: MGZero <onisephiroth@gmail.com>

---
+++ King of Thieves/KotException/KotInvalidActorException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace King_of_Thieves.KotException
+{
+    class KotInvalidActorException : System.Exception
+    {
+        public KotInvalidActorException(string message) :
+            base(message)
+        {
+            
+        }
+    }
+}
