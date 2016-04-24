30d21ff4ba9112a5b96cf5400483539db45c74d7
mgzero <onisephiroth@gmail.com>
2016-04-24 09:18:45 -0400

    added missing file
    Signed-off-by: mgzero <onisephiroth@gmail.com>

---
+++ King of Thieves/usr/local/MapTests/HryuleCastleTownTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace King_of_Thieves.usr.local.MapTests
+{
+    class HryuleCastleTownTest : PlayableState
+    {
+        protected override void _initMap()
+        {
+            CMasterControl.mapManager.swapMap("castleTown.xml", "player", new Vector2(400, 1000));
+        }
+    }
+}
